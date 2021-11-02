4733443e8a1760bac27b4d6dcc36fd75fc11acc6
Abhishek Jain <abhishek.jain4@shell.com>
2021-11-02 11:14:33 +0000

    bubble-sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,32 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let shouldCheck = true;
+  let count = 0;
+
+  while (shouldCheck && count < 10) {
+    shouldCheck = false;
+    for (let i = 0; i < nums.length - 1; i++) {
+      const num0 = nums[i];
+      const num1 = nums[i + 1];
+
+      if (num0 > num1) {
+        nums[i] = num1;
+        nums[i + 1] = num0;
+
+        shouldCheck = true;
+      }
+    }
+
+    count++;
+  }
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test.only("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
